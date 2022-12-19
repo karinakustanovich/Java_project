611c969ac16db43f768c8ada8efee31db64745a5
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2022-12-19 21:23:10 +0300

    Update project files

---
+++ src/variables.java
@@ -0,0 +1,29 @@
+public class variables {
+    public static void main(String[] args) {
+        byte myByte;
+        myByte = 4;
+        byte mybyte = -128;
+        System.out.println(myByte);
+        short myShort = 2343;
+
+        // myShort.soutv
+        System.out.println("myShort = " + myShort);
+        int myInt = 1231312321;
+        long myLong = 1312311232132132312L;
+
+        float myFloat = 4.56f;
+        double myDouble = 56.78;
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char myChar = '\u2691';
+        char myChar2 = 'g';
+        System.out.println("myChar = " + myChar);
+
+        String myName =  "Karina";
+        System.out.println("myName = " + myName);
+
+
+    }
+}

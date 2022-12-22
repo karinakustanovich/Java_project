84dbc7495e4c01e670cf467b389c08bb7556ecc6
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2022-12-22 20:52:29 +0300

    Create incrementdecrement.java

---
+++ src/lesson3/incrementdecrement.java
@@ -0,0 +1,28 @@
+package lesson3;
+
+public class incrementdecrement {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("a = " + a);
+        a = a + 5;
+        System.out.println("a = " + a);
+
+        a += 10;
+        System.out.println("a = " + a);
+
+        a = a + 1;
+        System.out.println("a = " + a );
+
+        a += 1;
+        System.out.println("a = " + a );
+
+        a++;
+        System.out.println("a = " + a );
+
+        a--;
+        System.out.println("a = " + a );
+
+
+
+    }
+}

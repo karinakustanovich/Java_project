5f8746b7e402681f66a340ace30b93a4a2de8e2c
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2022-12-26 20:54:55 +0300

    ok

--- src/lesson4/IfElse.java
+++ src/lesson4/IfElse.java
@@ -8,8 +8,9 @@ public class IfElse {
 
         System.out.println("Please enter your age...");
         int age = input.nextInt();
+        String whoAreYou;
 
-        if (age > 18) {
+/*        if (age > 18) {
             System.out.println("You're adult");
 
         }
@@ -19,6 +20,9 @@ public class IfElse {
         }
         else {
             System.out.println("You're young");
-        }
+        }*/
+
+        whoAreYou = (age == 18) ? "You're 18 years old" : (age > 18) ? "You're adult" : "You're young";
+        System.out.println("whoAreYou = " + whoAreYou);
     }
 }
---
+++ src/lesson4/Ternary.java
@@ -0,0 +1,26 @@
+package lesson4;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+        int c = 6;
+        String result;
+
+/*        if (a == b) {
+            result = "a and b are equal";
+            System.out.println("a and b are equal");
+        }
+
+        else {
+            result = "a and b are not equal";*/
+
+            result = (a == b) ? "a and b are equal" : "a and b are not equal";
+            System.out.println("result = " + result);
+
+            result = (a == c) ? "a and c are equal" : "a and c are not equal";
+            System.out.println("result = " + result);
+    }
+    }
+

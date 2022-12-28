941b4b9bcf4176c7fd53291f7f1d447690136d82
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2022-12-28 18:48:19 +0300

    ok

---
+++ src/lesson5/falseoperator.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class falseoperator {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("!a(true) = " + !a);
+        System.out.println("!b(false) = " + !b);
+
+    }
+}
---
+++ src/lesson5/operatoror.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class operatoror {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+        boolean d = false;
+
+        System.out.println(a + " || " + b + " = " + (a || b));
+        System.out.println(a + " || " + c + " = " + (a || c));
+        System.out.println(c + " || " + d + " = " + (c || d));
+    }
+}
